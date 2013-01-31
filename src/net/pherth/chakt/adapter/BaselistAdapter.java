b550242bcd4e302c613852dd0e70b42e3d5a0553
Phillip Thelen <viirus@pherth.net>
2013-01-31 02:18:42 +0100

    changed baselist from ListView to StickyListViewHeader

--- src/net/pherth/chakt/adapter/BaselistAdapter.java
+++ src/net/pherth/chakt/adapter/BaselistAdapter.java
@@ -7,10 +7,14 @@ import java.util.List;
 import net.pherth.chakt.R;
 import net.pherth.chakt.R.id;
 import net.pherth.chakt.R.layout;
+import net.pherth.chakt.adapter.ShowSeasonsAdapter.HeaderViewHolder;
+import net.pherth.chakt.adapter.ShowSeasonsAdapter.ViewHolder;
 
+import com.emilsjolander.components.stickylistheaders.StickyListHeadersAdapter;
 import com.jakewharton.trakt.TraktEntity;
 import com.jakewharton.trakt.entities.MediaBase;
 import com.jakewharton.trakt.entities.Movie;
+import com.jakewharton.trakt.entities.TvShowEpisode;
 
 import android.content.Context;
 import android.util.Log;
@@ -22,38 +26,83 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class BaselistAdapter extends ArrayAdapter<MediaBase> {
+public class BaselistAdapter extends ArrayAdapter<MediaBase>  implements StickyListHeadersAdapter {
 
+	LayoutInflater inflater;
+	
 	public BaselistAdapter(Context context) {
 	    super(context, R.layout.fragment_baselist);
+	    inflater = LayoutInflater.from(context);
 	}
 	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-	
-	    View v = convertView;
-	
-	    if (v == null) {
-	
-	        LayoutInflater vi;
-	        vi = LayoutInflater.from(getContext());
-	        v = vi.inflate(R.layout.listitem_baselist, null);
-	
-	    }
+		ViewHolder holder;
 
-    	MediaBase entity = this.getItem(position);
-	
-	    if (entity != null) {
-	    	Button checkinbutton = (Button) v.findViewById(R.id.checkinbutton);
-	        TextView titleView = (TextView) v.findViewById(R.id.title);
-	        TextView subinfoView = (TextView) v.findViewById(R.id.subinfo);
-	        
-	        titleView.setText(entity.title);
-	        subinfoView.setText(String.valueOf(entity.year));
-	    }
-	
-	    return v;
+		if (convertView == null) {
+			holder = new ViewHolder();
+			convertView = inflater.inflate(R.layout.listitem_seasonlist, parent, false);
+			holder.title = (TextView) convertView.findViewById(R.id.title);
+			holder.subinfo = (TextView) convertView.findViewById(R.id.subinfo);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+		
+		MediaBase entity = this.getItem(position);
+		
+		holder.title.setText(entity.title);
+		holder.subinfo.setText(entity.year.toString());
+		
+		return convertView;
 
 	}
+
+	@Override
+	public View getHeaderView(int position, View convertView, ViewGroup parent) {
+		HeaderViewHolder holder;
+		if (convertView == null) {
+			holder = new HeaderViewHolder();
+			convertView = inflater.inflate(R.layout.listitem_seasonlist_header, parent, false);
+			holder.text = (TextView) convertView.findViewById(R.id.text);
+			convertView.setTag(holder);
+		} else {
+			holder = (HeaderViewHolder) convertView.getTag();
+		}
+		char headerChar = getHeaderChar(this.getItem(position).title);
+		holder.text.setText(String.valueOf(headerChar));
+
+		return convertView;
+	}
+
+	//remember that these have to be static, postion=1 should walys return the same Id that is.
+	@Override
+	public long getHeaderId(int position) {
+		return getHeaderChar(this.getItem(position).title);
+	}
 	
+	private char getHeaderChar(String title) {
+		char headerChar;
+		//set header text as first char in name
+		
+		if(title.length() > 3) { 
+			if(title.subSequence(0, 3).toString().equalsIgnoreCase("the")) {
+				headerChar = title.subSequence(4, 5).charAt(0);
+			} else {
+				headerChar = title.subSequence(0, 1).charAt(0);
+			}
+		} else {
+			headerChar = title.subSequence(0, 1).charAt(0);
+		}
+		return headerChar;
+	}
+	
+	class HeaderViewHolder {
+		TextView text;
+	}
+
+	class ViewHolder {
+		TextView title;
+		TextView subinfo;
+	}
 }
