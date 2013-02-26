166940f780f795f7275ccef47fe7a6b9ec850574
Phillip Thelen <viirus@pherth.net>
2013-02-26 02:23:49 +0100

    New search activity properly registered in Manifest.

--- src/net/pherth/chakt/SearchActivity.java
+++ src/net/pherth/chakt/SearchActivity.java
@@ -11,7 +11,9 @@ import net.pherth.chakt.fragments.ShowProgressFragment;
 import net.pherth.chakt.fragments.ShowProgressFragment_;
 import net.pherth.chakt.fragments.ShowWatchlistFragment_;
 import net.pherth.chakt.fragments.WatchlistsFragment;
+import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
@@ -27,109 +29,26 @@ import com.google.inject.Inject;
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
 
+import de.keyboardsurfer.android.widget.crouton.Crouton;
+import de.keyboardsurfer.android.widget.crouton.Style;
+
 @EActivity(R.layout.activity_main)
 public class SearchActivity
     extends SherlockFragmentActivity
-    implements OnNavigationListener
 {
 	
-	private final List<ItemInfo> mItems = new ArrayList<ItemInfo>();
-	
-	class ItemInfo {
-        private final String tag;
-        private final Class<?> clss;
-        private final Bundle args;
-        private SherlockFragment fragment;
-
-        ItemInfo(String _tag, Class<?> _class, Bundle _args) {
-            tag = _tag;
-            clss = _class;
-            args = _args;
-        }
-    }
-	
-	@Inject Context context;
-	
-	@AfterViews
-    void afterViews() {
-        configureActionBar();
-    }
-	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		context = getApplicationContext();
-		
-    }
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getSupportMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-    private void configureActionBar() {
-        Context context = getSupportActionBar().getThemedContext();
-        ArrayAdapter<CharSequence> list = ArrayAdapter.createFromResource(context, R.array.locations, R.layout.sherlock_spinner_item);
-        list.setDropDownViewResource(R.layout.sherlock_spinner_dropdown_item);
-        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-        getSupportActionBar().setListNavigationCallbacks(list, this);
-        mItems.add(new ItemInfo("Show Progress", ShowProgressFragment.class, null));
-        mItems.add(new ItemInfo("Movie Watchlists", WatchlistsFragment.class, null));
-        mItems.add(new ItemInfo("Show Watchlists", WatchlistsFragment.class, null));
-        mItems.add(new ItemInfo("Episode Watchlists", WatchlistsFragment.class, null));
-        mItems.add(new ItemInfo("Lists", MovieWatchlistFragment.class, null));
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-    	Log.d("MainActivity", "Listitem selected");
-    	ItemInfo currItem = mItems.get(itemPosition);
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        switch(itemPosition) {
-        	case 0:
-        		Log.d("MainActivity", "Case 1");
-        		if(currItem.fragment == null) {
-        			currItem.fragment = ShowProgressFragment_.newInstance();
-        		}
-        		break;
-        	case 1:
-        		if(currItem.fragment == null) {
-        			currItem.fragment = MovieWatchlistFragment_.newInstance();
-        		}
-        		break;
-        		
-        	case 2:
-        		if(currItem.fragment == null) {
-        			currItem.fragment = ShowWatchlistFragment_.newInstance();
-        		}
-        		break;
-        	
-        	case 3:
-        		if(currItem.fragment == null) {
-        			currItem.fragment = EpisodeWatchlistFragment_.newInstance();
-        		}
-        		break;
-        	
-        	
-        	case 4:
-        		if(currItem.fragment == null) {
-        			currItem.fragment = MovieWatchlistFragment_.newInstance();
-        		}
-        		break;
-        }
-        ft.replace(R.id.maincontent, currItem.fragment).commitAllowingStateLoss();
-    	return true;
-    }
-    
-    @Override  
-    public void onConfigurationChanged(Configuration newConfig) {  
-      Log.d("MainActivity", "SCREEN ROTATED!");
-      if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {  
-
-      } else if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {  
-
-      }  
-    }  
-
+	    // Get the intent, verify the action and get the query
+	    Intent intent = getIntent();
+	    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+	      String query = intent.getStringExtra(SearchManager.QUERY);
+	      
+	    }
+	    
+	    Crouton.showText(this, "test", Style.ALERT);
+	    System.out.println("test123");
+	}
 }
